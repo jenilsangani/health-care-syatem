c94e915c4e089ad95121006506bbb63d5fc49ec6
jenilsangani <jenilsangani2000@gmail.com>
2022-09-07 10:38:32 +0530

    added animations

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,30 +1,34 @@
 "use strict";
 
-// let searchFrom = document.querySelector("aside");
 var isMenuOpen = true;
-var isMenuOpenFont = true;
 
 function toggleMenu() {
   var menuItems = document.querySelectorAll(".menuItemTitle");
-  console.log(menuItems);
   var aside = document.querySelector("aside");
+  var sidebar = document.querySelector("div.sidebar");
+  var downArrow = document.getElementById("downarrow");
+  var clz = document.getElementById("close-btn");
 
   if (isMenuOpen) {
     menuItems.forEach(function (menuItem) {
       menuItem.style.display = "none";
-      menuItem.style.alignItems = "center"; // menuItem.style.justifyContent = "center"; 
-      // menuItem.style.display = "flex"; 
-    });
+    }); // sidebar.style.width = "auto";
+
+    sidebar.style.width = "100px";
+    downArrow.style.display = "none";
+    clz.style.align_self = "center";
+    clz.style.margin = "auto";
     aside.style.width = "100px";
   } else if (!isMenuOpen) {
     menuItems.forEach(function (menuItem) {
       menuItem.style.display = "inline";
     });
+    sidebar.style.width = "290px";
+    downArrow.style.display = "block";
+    clz.style.align_self = "flex-end";
+    clz.style.margin = "19px 17px 0 auto";
     aside.style.width = "290px";
   }
 
   isMenuOpen = !isMenuOpen;
-} // function tiggleFont() {
-//   let font = document.querySelector(".heading");
-//   console.log(font);
-// }
+}
--- main.js
+++ main.js
@@ -1,32 +1,34 @@
-// let searchFrom = document.querySelector("aside");
-let isMenuOpen = true;
-let isMenuOpenFont = true;
+"use strict";
 
+var isMenuOpen = true;
 
 function toggleMenu() {
   let menuItems = document.querySelectorAll(".menuItemTitle");
-  console.log(menuItems);
   let aside = document.querySelector("aside");
+  let sidebar = document.querySelector("div.sidebar");
+  let downArrow = document.getElementById("downarrow");
+  let clz = document.getElementById("close-btn");
+
   if (isMenuOpen) {
-    menuItems.forEach((menuItem) => {
+    menuItems.forEach(function (menuItem) {
       menuItem.style.display = "none";
-      menuItem.style.alignItems = "center"; 
-      // menuItem.style.justifyContent = "center"; 
-      // menuItem.style.display = "flex"; 
-      
     });
+    // sidebar.style.width = "auto";
+    sidebar.style.width = "100px";
+    downArrow.style.display = "none";
+    clz.style.align_self = "center";
+    clz.style.margin = "auto";
     aside.style.width = "100px";
   } else if (!isMenuOpen) {
-    menuItems.forEach((menuItem) => {
+    menuItems.forEach(function (menuItem) {
       menuItem.style.display = "inline";
     });
+    sidebar.style.width = "290px";
+    downArrow.style.display = "block";
+    clz.style.align_self = "flex-end";
+    clz.style.margin = "19px 17px 0 auto";
     aside.style.width = "290px";
   }
+
   isMenuOpen = !isMenuOpen;
 }
-
-
-// function tiggleFont() {
-//   let font = document.querySelector(".heading");
-//   console.log(font);
-// }
